25e6d1af699d65e1a0c7f5f328986715e918ad0d
TheKnightliestKnightofKnightlyKnights <colter.hulse@western.edu>
2018-05-03 10:14:59 -0600

    Quick Update

--- app/src/main/java/edu/western/cs/starfinder_character_sheet/Character/Character.java
+++ app/src/main/java/edu/western/cs/starfinder_character_sheet/Character/Character.java
@@ -9,7 +9,7 @@ public class Character {
     int conAug;
     int wis;
     int wisAug;
-    int inteligence;
+    int intelligence;
     int intelligenceAug;
     int cha;
     int chaAug;
@@ -17,7 +17,7 @@ public class Character {
     int sp;
     int speed;
     int resolve;
-    int classLvl;
+    int totalLvl;
     int bab;
     int kac;
     int eac;
@@ -27,6 +27,175 @@ public class Character {
     int conSave;
     int willSave;
     int refSave;
+
+
+    public void setStr(int str){
+        this.str=str;
+    }
+    public int getStr()
+    {
+        return this.str;
+    }
+    public void setStrAug(int strAug){
+        this.strAug=strAug;
+    }
+    public int getStrAug()
+    {
+        return this.strAug;
+    }
+    public void setDex(int dex){
+        this.dex=dex;
+    }
+    public int getDex()
+    {
+        return this.dex;
+    }
+    public void setDexAug(int dexAug){
+        this.dexAug=dexAug;
+    }
+    public int getdexAug()
+    {
+        return this.dexAug;
+    }
+    public void setCon(int con){
+        this.con=con;
+    }
+    public int getCon()
+    {
+        return this.con;
+    }
+    public void setConAug(int conAug){
+        this.conAug=conAug;
+    }
+    public int getWis()
+    {
+        return this.wis;
+    }
+    public void setWis(int wis){
+        this.wis=wis;
+    }
+    public int getWisAug()
+    {
+        return this.wisAug;
+    }
+    public void setWisAug(int wisAug){
+        this.wisAug=wisAug;
+    }
+    public int getIntelligence()
+    {
+        return this.intelligence;
+    }
+    public void setIntelligence(int intelligence){
+        this.intelligence=intelligence;
+    }
+    public int getIntelligenceAug()
+    {
+        return this.intelligenceAug;
+    }
+    public void setIntelligenceAug(int intelligenceAug){
+        this.intelligenceAug=intelligenceAug;
+    }
+    public int getCha()
+    {
+        return this.cha;
+    }
+    public void setCha(int cha){
+        this.cha=cha;
+    }
+    public int getChaAug()
+    {
+        return this.chaAug;
+    }
+    public void setChaAug(int con){
+        this.chaAug=chaAug;
+    }
+    public int getHp()
+    {
+        return this.hp;
+    }
+    public void setHp(int hp){
+        this.hp=hp;
+    }
+    public int getSp()
+    {
+        return this.sp;
+    }
+    public void setSp(int sp){
+        this.sp=sp;
+    }
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+    public void setSpeed(int speed){
+        this.speed=speed;
+    }
+    public int getResolve()
+    {
+        return this.resolve;
+    }
+    public void setResolve(int resolve){
+        this.resolve=resolve;
+    }
+    public int getBab()
+    {
+        return this.bab;
+    }
+    public void setBab(int cha){
+        this.bab=bab;
+    }
+    public int getKac()
+    {
+        return this.kac;
+    }
+    public void setKac(int kac){
+        this.kac=kac;
+    }
+    public int getEac()
+    {
+        return this.eac;
+    }
+    public void setEac(int eac){
+        this.eac=eac;
+    }
+    public int getInit()
+    {
+        return this.init;
+    }
+    public void setinit(int init){
+        this.init=init;
+    }
+    public int getConSave()
+    {
+        return this.conSave;
+    }
+    public void setConSave(int conSave){
+        this.conSave=conSave;
+    }
+    public int getWIllSave()
+    {
+        return this.willSave;
+    }
+    public void setWillSave(int willSave){
+        this.willSave=willSave;
+    }
+    public int getRefSave()
+    {
+        return this.refSave;
+    }
+    public void setRefSave(int refSave){
+        this.refSave=refSave;
+    }
+    public int getTotalLvl()
+    {
+        return this.totalLvl;
+    }
+    public void setTotalLvl(int totalLvl)
+    {
+        this.totalLvl=totalLvl;
+    }
+    public String getName(){return this.name;}
+    public void setName(String name){this.name=name;}
 
 
 }
